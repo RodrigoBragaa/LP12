4bd880fc7be1cc08ca3a138209bd632a4afda220
Rodrigo Braga Guimarães Brito <87531169+RodrigoBragaa@users.noreply.github.com>
2023-04-06 09:06:57 -0300

    Add files via upload

---
+++ TP02 LP1/TP02/TP02Ex05.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+/**
+ * Nome: Rodrigo Braga Guimarães Brito	
+ * Nome: João Vitor Pedral do Nascimento
+ * 5.  Armazenar seis valores em uma matriz de ordem 3x2. Apresentar os valores na tela.
+ */
+
+public class TP02Ex05 {
+    
+    public static void main(String[] args){
+        final int linhas = 3, colunas = 2;
+
+        float[][] valores = new float[linhas][colunas];
+        Scanner scan = new Scanner(System.in);
+
+        for(int i = 0; i < valores.length; i++){
+            for(int j = 0 ; j < valores[i].length; j++){
+                System.out.print("Entre o valor da posição ["+ i +"]["+ j +"]: ");
+                valores[i][j] = scan.nextFloat();
+
+            }
+        }
+
+        System.out.println("\n\n============== Matriz ==============\n");
+        for(int i = 0; i < valores.length; i++){
+            for(int j = 0 ; j < valores[i].length; j++){
+                System.out.print(valores[i][j] + "   ");
+            }
+            System.out.println("");
+        }
+      
+        scan.close();
+    }
+}
